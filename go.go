6d45b87694b74bf02a8c44d9f62cac7eaa5f6598
Max <erosspd@gmail.com>
2021-10-23 15:18:23 +0300

    new code >> future >> Max QA Automation engeneer

--- go.go
+++ go.go
@@ -4,5 +4,11 @@ import "fmt"
 
 func main() {
 	pro := "Макс - программист"
-	fmt.Println(pro)
+	a := "автоматизатор"
+	w := "c"
+	y := 2022
+	yy := "года"
+
+	fmt.Println(pro, a, w, y, yy)
+	fmt.Printf("%T %T %T %T %T", pro, a, w, y, yy) // функция выводит типы
 }
