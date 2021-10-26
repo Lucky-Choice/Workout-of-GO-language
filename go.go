22c1d27404dbf60516c5d84365b25003c9c92d80
Max <erosspd@gmail.com>
2021-10-26 15:56:07 +0300

    NEW function for search files in detectory in home PC

--- go.go
+++ go.go
@@ -1,30 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
 
 func main() {
-	i := 1
-	for i <= 10 {
-		fmt.Println(i)
-		i = i + 1
+	// i := 1
+	// for i <= 10 {
+	// 	fmt.Println(i)
+	// 	i = i + 1
+	// }
+
+	// for i := 0; i <= 7; i++ { // упрошенный вариант первого цикла
+	// 	fmt.Println(i)
+	// }
+
+	// text := []string{"this", "is", "learn", "Go", "for", "me"}
+	// fmt.Println(text[3])     // вывесть конкретное значение из массива по его индексу
+	// for _, i := range text { // вывести все значения массива по порядку
+	// 	fmt.Println(i)
+	// }
+
+	// text1 := []string{"this", "is", "learn", "Go", "for", "me"}
+	// for _, i := range text1 {
+	// 	if i == "Go" { // если значение равно "Go" то цикл заканчивается. Значение Go не выведится т.к. i == "Go"
+	// 		break
+	// 	}
+	// 	fmt.Println(i)
+	// }
+
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	for i := 0; i <= 7; i++ { // упрошенный вариант первого цикла
-		fmt.Println(i)
-	}
-
-	text := []string{"this", "is", "learn", "Go", "for", "me"}
-	fmt.Println(text[3])     // вывесть конкретное значение из массива по его индексу
-	for _, i := range text { // вывести все значения массива по порядку
-		fmt.Println(i)
-	}
-
-	text1 := []string{"this", "is", "learn", "Go", "for", "me"}
-	for _, i := range text1 {
-		if i == "Go" { // если значение равно "Go" то цикл заканчивается. Значение Go не выведится т.к. i == "Go"
-			break
-		}
-		fmt.Println(i)
+	for _, file := range files {
+		fmt.Println(file.Name())
 	}
 
 }
